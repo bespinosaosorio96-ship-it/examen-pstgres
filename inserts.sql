61e58965e5995bfe9a26d165a72adef3ce80016e
Brayan Espinosa Osorio <228817178+bespinosaosorio96-ship-it@users.noreply.github.com>
2026-06-30 21:12:47 -0500

    Add initial data inserts for vehicles, employees, and sales

---
+++ inserts.sql
@@ -0,0 +1,42 @@
+
+insert into vehiculos (marca,modelo,año,estado,precio)values ('mazda','mx5',2027,'Nuevo',178985000.99);
+insert into vehiculos (marca,modelo,año,estado,precio)values 
+('mazda','cx30',2026,'Usado',188898500.99),
+('renault','clio',2018,'Nuevo',28000000),
+('toyota','Tacoma',2027,'Nuevo',520000000);
+
+
+insert into empleados (nombre,identificacion,rol,contratacion) values ('Brayan Espinosa','123456789','Vendedor','2022/12/14');
+
+insert into empleados (nombre,identificacion,rol,contratacion) values 
+('Camilo Albarracin','123856789','Service','2024/5/26'),
+('Andres Florez','123897645','Service','2020/6/20'),
+('Aquiles Vendo','456789123','Vendedor','2021/7/30');
+
+
+insert into provedores (nombre) values ('Mitsuishi'),('bmw'),('mazda');
+
+
+
+insert into ventas_vendedor (id_vendedor,id_vehiculo,ventas_generadas) values
+ (1,1,1200000000),
+(4,4,8000000)
+;
+
+
+insert into clientes (nombre,identificacion,telefono,fecha_contacto,tipo) values ('Brayan','789456','123456','2023/8/4','Comprador');
+
+insert into clientes (nombre,identificacion,telefono,fecha_contacto,tipo) values
+('Camilo','7894565','78123456','2025/8/4','Comprador'),
+('Andres','45678912','256981','2026/5/12','Potencial'),
+('Carlos','8963547','589663315','2024/5/31','Potencial');
+
+
+
+
+
+insert into historial_servicios (servicio,fecha,id_cliente ,id_empleado,id_vehiculo,marca_vehiculo,modelo_vehiculo) values
+
+
+
+
