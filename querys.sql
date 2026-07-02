5a31d92a626244bd6c1338d297654be0b1ad8b1f
Brayan Espinosa Osorio <228817178+bespinosaosorio96-ship-it@users.noreply.github.com>
2026-07-01 19:12:16 -0500

    Refactor SQL queries for better data insights
    Updated SQL queries to improve data retrieval for various reports.

--- querys.sql
+++ querys.sql
@@ -6,61 +6,83 @@ select * from vehiculos;
 
 -- 2.  Mostrar los clientes que han realizado compras recientemente, junto con la información de los vehículos adquiridos.
 
-select nombre , fecha_contacto, v.marca ,v.modelo  
-from clientes c
-inner join vehiculos v on v.id = c.id
-order by fecha_contacto desc;
-
+select c.nombre as cliente, v.marca, v.modelo, vv.fecha_venta
+from ventas_vendedor vv
+inner join clientes c
+on  vv.id_cliente=c.id
+inner join vehiculos v
+on  vv.id_vehiculo=v.id
+ORDER BY vv.fecha_venta DESC;
 --3.Obtener el historial completo de servicios realizados para un vehículo específico, incluyendo detalles sobre los empleados involucrados y las fechas de servicio.
 
-
-select hs.id, hs.servicio , v.marca , v.modelo 
+select v.marca, v.modelo, hs.servicio, hs.fecha, e.nombre as empleado
 from historial_servicios hs
-inner join vehiculos v on v.id = hs.id 
-;
+inner join vehiculos v
+on hs.id_vehiculo=v.id
+inner join empleados e
+on hs.id_empleado=e.id
+where v.id=1;
 
 --4 istar los proveedores de piezas que han suministrado componentes utilizados en los servicios de mantenimiento.
 
-select provedor 
-from historial_servicios hs
-where provedor != null;
 
+select distinct p.nombre
+from historial_servicios hs
+inner join proveedores p
+on hs.id_proveedor=p.id;
 --5 Calcular las comisiones generadas por cada empleado del departamento de ventas en un período específico.
 
-select ventas_generadas , ventas_generadas*0.2 as comision, e.nombre 
-from ventas_vendedor vv 
-inner join empleados e on e.id=vv.id;
+select e.nombre,  SUM(vv.valor_venta*0.05) as comision 
+from ventas_vendedor vv
+inner join empleados e
+on vv.id_vendedor=e.id
+where vv.fecha_venta between '2025-01-01' and '2025-12-31'
+group by e.nombre;
+
 
 
 --6Identificar todos los servicios de mantenimiento realizados por un empleado específico, incluyendo detalles sobre los vehículos atendidos.
 
-
-select *
-from historial_servicios hs 
-where id_empleado =1;
+select e.nombre, v.marca, v.modelo , hs.servicio, hs.fecha
+from historial_servicios hs
+inner join empleados e
+on hs.id_empleado=e.id
+inner join vehiculos v
+on hs.id_vehiculo=v.id
+where e.id=2;
 -- Mostrar información sobre los clientes potenciales y los vehículos de su interés, proporcionando pistas valiosas para estrategias de marketing.
 
-select *
-from clientes c 
-inner join vehiculos v on c.id=v.id 
-where tipo='Potencial';
+select c.nombre, v.marca, v.modelo, ic.fecha_interes 
+from interes_clientes ic
+inner join clientes c
+on ic.id_cliente=c.id
+inner join vehiculos v
+on ic.id_vehiculo=v.id
+where c.tipo='Potencial';
 
 --8istar todos los empleados que pertenecen al departamento de servicio, junto con sus horarios de trabajo.
-
-select *
-from departamento_servicios ds ;
+select e.nombre, ds.servicio, ds.horario
+from departamento_servicios ds
+inner join empleados e
+on ds.id_empleado=e.id;
 
 --ncontrar los vehículos vendidos en un rango de precios específico, proporcionando datos útiles para análisis de ventas.
-select *
-from vehiculos v 
-inner join ventas_vendedor vv on v.id =vv.id_vehiculo 
-where precio between 120000000 and 300000000;
+select v.marca, v.modelo, vv.valor_venta
+from ventas_vendedor vv
+inner join vehiculos v
+on vv.id_vehiculo=v.id
+where vv.valor_venta between 100000000 and 600000000;
 
 
 --10.Identificar a aquellos clientes que han realizado más de una compra en el concesionario, destacando la lealtad del cliente.
 
 
-
+select c.nombre, count(vv.id) as total_compras
+from clientes c
+inner join ventas_vendedor vv
+on c.id=vv.id_cliente
+group by c.nombre
+having count(vv.id)>1;
 
 
 
